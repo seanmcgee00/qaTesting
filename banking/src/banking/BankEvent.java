416eac903e541fb4a153fdf9db74afdbd71d202b
Sean McGee <d00121058@student.dkit.ie>
2018-02-12 21:36:52 +0000

    Banking App

---
+++ banking/src/banking/BankEvent.java
@@ -0,0 +1,332 @@
+package banking;
+
+import java.awt.Button;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class BankEvent implements ActionListener  {
+	 static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	 static final String DB_URL = "jdbc:mysql://localhost/banking";
+	 static final String USER = "root";
+	 static final String PASS = "";
+	 JFrame currentFrame;
+	 Connection conn = null;
+	 Statement stmt = null;
+	 ButtonGroup accountType,gender;
+	 TextField name, address, accountNo, accountTypeFeild,genderFelid,currentBalance,amount;
+	 String myAccount;
+	 Double balance;
+	 
+	 
+
+	public BankEvent(ButtonGroup accountType, ButtonGroup gender, TextField name, TextField address, JFrame currentFrame) {
+	this.accountType=accountType;
+	this.gender= gender;
+	this.name=name;
+	this.address=address;
+	this.currentFrame=currentFrame;
+	}
+
+	public BankEvent(TextField accNo, TextField genderFelid,TextField typeFeild, TextField name, TextField address,TextField currentBalance, JFrame currentFrame) {
+		// TODO Auto-generated constructor stub
+		this.accountNo=accNo;
+		this.genderFelid= genderFelid;
+		this.accountTypeFeild=typeFeild;
+		this.name=name;
+		this.address=address;
+		this.currentBalance=currentBalance;
+		this.currentFrame=currentFrame;
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent x) {
+		// TODO Auto-generated method stub
+		Button y=(Button)x.getSource();
+		String buttonName=y.getLabel();
+		System.out.println(buttonName);
+		switch(buttonName){
+		case "Generate Account": accountInsert();
+		break;
+		
+		case "Get Account": getDetails();
+		break;
+		
+		
+		}
+		
+	}
+	
+	public void accountInsert()
+	{
+		String sql;
+		String accountNo="";
+		ResultSet rs = null;
+		String name = this.name.getText();
+		String address = this.address.getText();
+		String acc =this.accountType.getSelection().getActionCommand();
+		String gender=this.gender.getSelection().getActionCommand();
+		 try {
+			 Class.forName("com.mysql.jdbc.Driver");
+			 conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			 stmt = conn.createStatement();
+		
+		     sql ="select concat('"+acc+"','"+gender+"', LPAD(ifnull(max(substring(accountno,3,3))+1,'001'),3,'0'))from accounts where substring(accountno,1,1)='"+acc+"'";
+		     System.out.println("SQL= "+sql);
+		     rs = stmt.executeQuery(sql);
+		     while(rs.next()){
+		    	 accountNo=rs.getString(1);
+		    System.out.println("account no= "+rs.getString(1));
+
+		      }
+		     
+		     sql="insert into accounts values('"+accountNo+"','"+name+"','"+address+"')";
+		     stmt.executeUpdate(sql);
+		     JOptionPane.showMessageDialog(currentFrame,"Account No:"+accountNo+" has been Created");
+		     
+		     rs.close();
+		     stmt.close();
+		     conn.close();
+		     reset();
+		     currentFrame.dispose();
+			 
+		 }
+		 catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		   }catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   }
+		
+	
+	}
+	
+
+	public void getDetails()
+	{
+		ResultSet rs = null;
+		this.myAccount= this.accountNo.getText();
+		String sql="select a.accountNo,a.name,a.address, substring(a.accountno,1,1)as type,substring(a.accountno,2,1)as gender, ((select sum(amount)from transactionLog where accountno='"+myAccount+"' and actionTaken='D')-(select sum(amount)from transactionLog where accountno='"+myAccount+"' and actionTaken='W'))as balance \r\n" + 
+				"from accounts a,transactionLog t \r\n" + 
+				"where a.accountNo=t.accountNo and a.accountNo='"+myAccount+"'\r\n" + 
+				"GROUP by a.accountNo";
+		
+		 
+		 try {
+			 Class.forName("com.mysql.jdbc.Driver");
+			 conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			 stmt = conn.createStatement();
+		     rs = stmt.executeQuery(sql);
+		    if(!rs.isBeforeFirst())
+		    {
+		    	reset();
+		    	 JOptionPane.showMessageDialog(currentFrame,"Account No:"+myAccount+" does not Exsist");
+		    }
+		     while(rs.next()){
+		    	
+		    System.out.println("account no= "+rs.getString(1));
+		   this.accountNo.setText(rs.getString(1));
+		   this.name.setText(rs.getString(2));
+		   this.address.setText(rs.getString(3));
+		   
+		  if(rs.getString(5).equalsIgnoreCase("M"))
+		  {
+			  this.genderFelid.setText("Male");
+		  }
+		  else
+		  {
+			  this.genderFelid.setText("Female");
+		  }
+		  
+		  if(rs.getString(4).equalsIgnoreCase("S"))
+		  {
+			  this.accountTypeFeild.setText("Saving");
+		  }
+		  else
+		  {
+			  this.accountTypeFeild.setText("Current");
+		  }
+		   
+		   
+		   this.balance=(double) rs.getInt(6);
+		   this.currentBalance.setText(""+ this.balance);
+
+		      }
+		     
+		     stmt.close();
+		     conn.close();
+			 
+		 }
+		 catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		   }catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   }
+	
+	}
+	
+	public void deposit(TextField amount){
+		
+		String sql;
+		
+		this.amount=amount;
+		double amountDouble = Double.parseDouble(this.amount.getText());
+		
+		 try {
+			 Class.forName("com.mysql.jdbc.Driver");
+			 conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			 stmt = conn.createStatement();
+		     
+		     sql="insert into transactionlog(actionTaken,accountNo,amount) values('D','"+myAccount+"',"+amountDouble+")";
+		     System.out.println("the deposit sql is "+sql);
+		     stmt.executeUpdate(sql);
+		     JOptionPane.showMessageDialog(currentFrame,"€"+amountDouble+" has been deposited into Account No: "+myAccount.toUpperCase()+". Your new balance is"+(this.balance+amountDouble));
+		     
+		 
+		     stmt.close();
+		     conn.close();
+		     reset();
+		     currentFrame.dispose();
+			 
+		 }
+		 catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		   }catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   }
+		
+		
+	}
+	
+	
+	
+public void withdraw(TextField amount){
+		
+		String sql;
+		this.amount=amount;
+		double amountDouble = Double.parseDouble(this.amount.getText());
+		double newBalance=(this.balance-amountDouble);
+		System.out.println("Balance:"+this.balance+" amount:"+amountDouble);
+		
+		 try {
+			 Class.forName("com.mysql.jdbc.Driver");
+			 conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			 stmt = conn.createStatement();
+		     if(newBalance>=0)
+		     {
+		    	 
+		    	 System.out.println("New Balance:"+newBalance);
+		    	 sql="insert into transactionlog(actionTaken,accountNo,amount) values('W','"+myAccount+"',"+amountDouble+")";
+			     System.out.println("the deposit sql is "+sql);
+			     stmt.executeUpdate(sql);
+			     JOptionPane.showMessageDialog(currentFrame,"€"+amountDouble+" has been Withdrawn from Account No:"+myAccount.toUpperCase()+". Your new balance is €"+newBalance);
+			    
+			     reset();
+			     currentFrame.dispose();
+		     }
+		     else{
+			     JOptionPane.showMessageDialog(currentFrame,"You Have Insufficient Funds in your account to make this Withdrawl. Please Try a new amount!" ,"Inane error",
+			    		    JOptionPane.ERROR_MESSAGE);
+			     this.amount.setText("");
+		     }
+		     
+		     stmt.close();
+		     conn.close();
+		     
+			 
+		 }
+		 catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		   }catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   }
+		
+		
+	}
+
+public String transactionPanel(){
+	String transaction = "";
+	
+	ResultSet rs = null;
+	String sql;
+	String newline = "\n";
+	String tab="\t";
+
+
+	 try {
+		 Class.forName("com.mysql.jdbc.Driver");
+		 conn = DriverManager.getConnection(DB_URL,USER,PASS);
+		 stmt = conn.createStatement();
+	     
+	    
+	    	 sql="select actionTaken, amount, DATE_FORMAT(transactionDate, ' %d %m %y') from transactionlog where accountno='" +myAccount+"'";
+		     System.out.println("the deposit sql is "+sql);
+		     rs=stmt.executeQuery(sql);
+		    
+		     transaction=transaction+"Date" + tab+ "Action"+tab+ "Amount"+newline;
+		 
+		     while(rs.next()){
+			   
+		    	 transaction=transaction+rs.getString(3)+tab+ rs.getString(1)+tab+ +rs.getInt(2)+newline;
+			    
+			     System.out.println("DATE: "+rs.getString(3)+" ACTION:"+rs.getString(1)+" AMOUNT:"+rs.getInt(2));
+		    	
+				      }
+	     
+	     rs.close();
+	     stmt.close();
+	     conn.close();
+ 
+	 }
+	 catch(SQLException se){
+	      //Handle errors for JDBC
+	      se.printStackTrace();
+	   }catch(Exception e){
+	      //Handle errors for Class.forName
+	      e.printStackTrace();
+	   }
+
+	return transaction;
+}
+
+
+
+	public void reset() {
+		
+		 
+		 this.name.setText("");
+		 this.address.setText("");
+		 this.accountNo.setText("");
+		 this.genderFelid.setText("");
+		 this.accountTypeFeild.setText("");
+		 this.currentBalance.setText("");
+		 this.amount.setText("");
+	if (this.accountType != null)	 this.accountType.clearSelection();
+	if (this.gender != null)	 this.gender.clearSelection();	
+	}
+
+	
+
+}
---
+++ banking/src/banking/MainClass.java
@@ -0,0 +1,272 @@
+package banking;
+
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+public class MainClass {
+
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		JFrame  mainFrame= new JFrame("Bank of Ireland");
+		
+		JMenuBar mainMenu = new JMenuBar();
+		JMenu accounts= new JMenu("Accounts");
+		JMenu transactions= new JMenu("Transactions");
+		JMenuItem open = new JMenuItem("Open");
+		JMenuItem details = new JMenuItem("Details");
+		JMenuItem deposit = new JMenuItem("Deposit");
+		JMenuItem withdrawl = new JMenuItem("Withdrawl");
+		JMenuItem balance = new JMenuItem("Balance");
+		
+		JPanel transactionsPanel=new JPanel();
+		Panel detailsPanel= new Panel();
+		detailsPanel.setLayout(new GridLayout(2,2));
+		
+		Label nameLabel = new Label("Name");
+		Label abbressLabel = new Label("Address");
+		TextField name= new TextField(20);
+		TextField address= new TextField(20);
+		detailsPanel.add(nameLabel);
+		detailsPanel.add(name);
+		detailsPanel.add(abbressLabel);
+		detailsPanel.add(address);
+		
+		
+		Panel accNoPanel= new Panel();
+		accNoPanel.setLayout(new FlowLayout());
+		Label accLabel = new Label("Account No");
+		TextField accNo= new TextField(7);
+		Button getDetails= new Button("Get Account");
+		accNoPanel.add(accLabel);
+		accNoPanel.add(accNo);
+		accNoPanel.add(getDetails);
+		
+		Panel accInfoPanel= new Panel();
+		accInfoPanel.setLayout(new GridLayout(4,2));
+		Label genderLabel = new Label("Gender");
+		Label typeLabel = new Label("Account Type");
+		Label balanceLabel = new Label("Current Balance");
+		TextField genderFelid= new TextField(10);
+		TextField typeFeild= new TextField(10);
+		TextField currentBalance= new TextField(10);
+		accInfoPanel.add(genderLabel);
+		accInfoPanel.add(genderFelid);
+		accInfoPanel.add(typeLabel);
+		accInfoPanel.add(typeFeild);
+		accInfoPanel.add(balanceLabel);
+		accInfoPanel.add(currentBalance);
+		
+		Panel tPanel= new Panel();
+		tPanel.setLayout(new FlowLayout());
+		Label amountLabel = new Label("Amoumt");
+		TextField amount= new TextField(7);
+		Button depositButton= new Button();
+		tPanel.add(amountLabel);
+		tPanel.add(amount);
+		
+		
+		
+		
+		
+		
+		open.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				JFrame openFrame =new JFrame("Open Account");
+				Panel radioPanel= new Panel();
+				radioPanel.setLayout(new GridLayout(3,2));
+				JRadioButton male = new JRadioButton ("Male");
+				male.setActionCommand("M");
+				JRadioButton female = new JRadioButton ("female");
+				female.setActionCommand("F");
+				JRadioButton current = new JRadioButton ("Current");
+				current.setActionCommand("C");
+				JRadioButton saving = new JRadioButton ("Saving");
+				saving.setActionCommand("S");
+				
+				Label genderLabel = new Label("Gender");
+				Label accountTypeLabel = new Label("Account Type");
+				
+				ButtonGroup gender = new ButtonGroup();
+				ButtonGroup accountType = new ButtonGroup();
+				gender.add(male);
+				gender.add(female);
+				accountType.add(saving);
+				accountType.add(current);
+				radioPanel.add(genderLabel);
+				radioPanel.add(accountTypeLabel);
+				radioPanel.add(male);
+				radioPanel.add(current);
+				radioPanel.add(female);
+				radioPanel.add(saving);
+				
+				Button generate= new Button("Generate Account");
+				BankEvent insertPress= new BankEvent(accountType,gender,name,address,openFrame);
+			
+				generate.addActionListener(insertPress);
+			
+				openFrame.add(detailsPanel,BorderLayout.NORTH);
+				openFrame.add(radioPanel,BorderLayout.CENTER);
+				openFrame.add(generate,BorderLayout.SOUTH);
+				openFrame.setSize(400, 200);
+				openFrame.setLocation(700, 300);
+				openFrame.setVisible(true);
+				
+			}
+			
+		});
+		
+
+		
+		deposit.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				JFrame depositFrame= new JFrame("Deposit");
+				accInfoPanel.add(tPanel);
+				accInfoPanel.add(depositButton);
+				depositFrame.add(accNoPanel,BorderLayout.NORTH);
+				depositFrame.add(detailsPanel,BorderLayout.CENTER);
+				depositFrame.add(accInfoPanel,BorderLayout.SOUTH);
+				depositFrame.setSize(400, 300);
+				depositFrame.setLocation(700, 300);
+				depositFrame.setVisible(true);
+				BankEvent getAcc= new BankEvent(accNo,genderFelid,typeFeild,name,address,currentBalance,depositFrame);
+				
+				getDetails.addActionListener(getAcc);
+				depositButton.setLabel("Deposit");
+				depositButton.addActionListener( new ActionListener(){
+
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						// TODO Auto-generated method stub
+						getAcc.deposit(amount);
+					}});
+				
+			}
+			
+		});
+		
+		
+		withdrawl.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				JFrame depositFrame= new JFrame("Withdrawl");
+				accInfoPanel.add(tPanel);
+				accInfoPanel.add(depositButton);
+				depositFrame.add(accNoPanel,BorderLayout.NORTH);
+				depositFrame.add(detailsPanel,BorderLayout.CENTER);
+				depositFrame.add(accInfoPanel,BorderLayout.SOUTH);
+				depositFrame.setSize(400, 300);
+				depositFrame.setLocation(700, 300);
+				depositFrame.setVisible(true);
+				BankEvent getAcc= new BankEvent(accNo,genderFelid,typeFeild,name,address,currentBalance,depositFrame);
+				
+				getDetails.addActionListener(getAcc);
+				depositButton.setLabel("Withdraw");
+				depositButton.addActionListener( new ActionListener(){
+
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						// TODO Auto-generated method stub
+						getAcc.withdraw(amount);
+					}});
+				
+			}
+			
+		});
+		
+		details.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				JFrame detailsFrame= new JFrame("Details");
+				JTextArea transDetails= new JTextArea(5,50);
+				transDetails.setEditable(false);
+				transDetails.append("Transaction History \n");
+				JScrollPane scrollPane = new JScrollPane(transDetails);
+				
+				
+				BankEvent getAcc= new BankEvent(accNo,genderFelid,typeFeild,name,address,currentBalance,detailsFrame);
+				getDetails.addActionListener(getAcc);
+				getDetails.addActionListener(new ActionListener(){
+					
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						// TODO Auto-generated method stub
+						
+						transDetails.append(getAcc.transactionPanel());
+					   
+					   
+					    
+					    
+					    
+					   
+					}});
+				//Label header= new Label("Transaction History");
+				
+				
+				
+				 scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+				 scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+				
+				 transactionsPanel.add(scrollPane);
+				detailsFrame.add(accNoPanel,BorderLayout.NORTH);
+				detailsFrame.add(detailsPanel,BorderLayout.CENTER);
+				detailsFrame.add(transactionsPanel,BorderLayout.SOUTH);
+				detailsFrame.add(accInfoPanel,BorderLayout.EAST);
+				detailsFrame.setSize(600, 300);
+				detailsFrame.setLocation(700, 300);
+				detailsFrame.setVisible(true);
+			
+			
+				
+			}
+			
+		});
+		
+		
+		
+		
+		accounts.add(open);
+		accounts.add(details);
+		transactions.add(deposit);
+		transactions.add(withdrawl);
+		transactions.add(balance);
+		mainMenu.add(accounts);
+		mainMenu.add(transactions);
+		
+		
+		mainFrame.add(mainMenu,BorderLayout.NORTH);
+		mainFrame.setSize(400, 200);
+		mainFrame.setLocation(300, 300);
+		mainFrame.setVisible(true);
+		
+		
+	}
+}
+
+
